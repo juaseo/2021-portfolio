098f13b70a67e79ac5ac6c2606aad899066aa92a
booldook <booldook@gmail.com>
2021-06-22 18:00:04 +0900

    booldook

--- js/index.js
+++ js/index.js
@@ -1,11 +1,11 @@
  /*************** 글로벌 설정 *****************/
- let x = 0;
- let y = 0;
- let mx = 0;
- let my = 0;
- let speed = 0.01;
- let $picWrapper = $('.pic-wrapper img');
- let $picWrap = $('.pic-wrap img')
+ var x = 0;
+ var y = 0;
+ var mx = 0;
+ var my = 0;
+ var speed = 0.1;
+ var $picWrapper = $('.char-bg');
+ var $picWrap = $('.char')
 
 
 
@@ -36,22 +36,19 @@
 
 
 
-
-window.onload = function() {
-	$(this).mousemove(function (e) { 
-		x = (e.clientX - window.innerWidth / 2);
-		y = (e.clientY - window.innerWidth / 2);
-	}
-	loop();
-};
+$(window).mousemove(function (e) {
+  x = (e.clientX - window.innerWidth / 2);
+  y = (e.clientY - window.innerWidth / 2);
+})
+ loop()
 
 
 function loop() {
 	mx = ( x - mx ) * speed;
 	my = ( y - mx ) * speed;
-
-	$picWrapper.css({"transfrom": "translate("+ -(mx/3) +"px,"+ -(my/6) +"px)"});
-	$picWrap.css({"transfrom": "translate("+ -(mx/6) +"px,"+ -(my/4) +"px)"});
+  console.log(mx, my)
+	$picWrapper.css("transform", "translate("+ -(mx/3) +"px,"+ -(my/6) +"px)");
+	$picWrap.css("transform", "translate("+ -(mx/6) +"px,"+ -(my/4) +"px)");
 	
 	window.requestAnimationFrame(loop);
 }
