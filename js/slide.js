e01a467b01acf2f93ebd08d8e6e5564ac9cca660
booldook <booldook@gmail.com>
2021-06-28 16:22:59 +0900

    booldook

---
+++ js/slide.js
@@ -0,0 +1,48 @@
+var slide = [];
+var now = 0;
+var list = 0;
+$.get('../json/slide.json', onGetData);
+function onGetData(r) {
+	slide = r.slide;
+	now = 0;
+	last = slide.length - 1;
+	console.log(slide)
+}
+
+
+
+$('.slide-wrapper .bt-prev').click(onPrev);
+$('.slide-wrapper .bt-next').click(onNext);
+$(".slide-wrapper").swipe({ threshold: 30, swipe: onSwipe });
+
+function onSwipe(event, direction, distance, duration, fingerCount, fingerData) {
+	if(direction === 'left') $('.slide-wrapper .bt-prev').trigger('click');
+	else $('.slide-wrapper .bt-next').trigger('click');
+}
+
+function onPrev() {
+	now = now == 0 ? last : now - 1;
+	$('.slide-wrapper').addClass('active');
+	setTimeout(function(){
+		changeSlide();
+		$('.slide-wrapper').removeClass('active');
+	}, 775)
+}
+
+function onNext() {
+	now = now == last ? 0 : now + 1;
+	$('.slide-wrapper').addClass('active');
+	setTimeout(function(){
+		changeSlide();
+		$('.slide-wrapper').removeClass('active');
+	}, 775)
+}
+
+function changeSlide() {
+	console.log(slide[now])
+	$('.slide-wrapper').find('.title').text(slide[now].title);
+	$('.slide-wrapper').find('.content').text(slide[now].cont);
+	$('.slide-wrapper').find('.img-back').attr('src', slide[now].src);
+	$('.slide-wrapper').find('.img-front').attr('src', slide[now].src);
+	$('.slide-wrapper').find('.box').css('background-color', slide[now].box);
+}
